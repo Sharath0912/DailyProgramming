6b725942d9ebe140f272ca4312297a83012aa185
Sharath0912 <sharaths0901@gmail.com>
2024-04-23 06:52:15 +0530

    Linear Search

--- src/PatternProblems/Pattern4.java
+++ src/PatternProblems/Pattern4.java
@@ -4,7 +4,7 @@ public class Pattern4
 {
 	public static void main(String[] args) 
 	{
-		int n=3;
+		int n=5;
 		for(int i=1;i<=n;i++)
 		{
 			for(int j=1;j<=n;j++)
---
+++ src/Searching/LinearSearch.java
@@ -0,0 +1,45 @@
+package Searching;
+
+import java.util.Scanner;
+
+public class LinearSearch 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Length Of the Array : ");
+		int n=sc.nextInt();
+		
+		int arr[]=new int[n];
+		
+		System.out.println("Enter the Elements : "); 
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("Enter the Key Element to Search : ");
+		int key=sc.nextInt();
+		
+		int index=linearSearch(arr,key);
+		
+		if(index != -1)
+		{
+			System.out.println("Element Found At "+index+"...!");
+		}
+		else
+			System.out.println("Element Not Found....!");
+		
+	}
+
+	public static int linearSearch(int[] arr, int key) 
+	{
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]==key)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+}
