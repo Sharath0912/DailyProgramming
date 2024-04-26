80488b90e2e10960f962d68a8d12bca43230a39c
Sharath0912 <sharaths0901@gmail.com>
2024-04-26 19:36:59 +0530

    Pattern6

--- src/PatternProblems/Pattern6.java
+++ src/PatternProblems/Pattern6.java
@@ -1,5 +1,22 @@
 package PatternProblems;
 
-public class Pattern6 {
+import java.util.Scanner;
 
+public class Pattern6 
+{
+	public static void main(String[] args) 
+	{
+		
+		int n= 5;
+		int k= 1;
+		for(int i=1;i<=n;i++)
+		 {
+			 for(int j=1;j<=n;j++)
+			 {
+				 System.out.printf("%2d ",k);
+				 k++;
+			 }
+			 System.out.println();
+		 }
+	}
 }
